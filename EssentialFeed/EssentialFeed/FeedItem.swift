49b2745bc22a6dbf07a93e15ed3c8e9df9d49650
Erick <erick@tech.fit>
2021-06-21 23:01:18 -0300

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Erick Yamato on 21/06/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
